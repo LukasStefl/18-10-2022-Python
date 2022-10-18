172f7243d4783f673910e6a1aa47c8827cfbefef
LukasStefl <lukas.stefl@student.ossp.cz>
2022-10-18 10:58:57 +0200

    commit

--- Druhy_ukol_hadani.py
+++ Druhy_ukol_hadani.py
@@ -3,3 +3,7 @@ number = random.randint(0,11)
 n = 0
 while (number != n):
     n = int(input());
+    if n == number:
+        print("\nUhodl jsi\n");
+    elif n != number:
+        print("\nHádej znovu.\n");
